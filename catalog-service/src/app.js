d54c7088e31805c20d06fd5f46a25b116e93661f
carlos-cas3 <ccastillob44@gmail.com>
2026-05-07 22:31:25 -0500

    catalog-service | .js and schema db

---
+++ catalog-service/src/app.js
@@ -0,0 +1,19 @@
+const express= require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Catalog Service funcionando"
+    })
+})
+
+const PORT = 3001
+
+app.listen(PORT,() => {
+    console.log(`Servidor ejecutandose en puerto ${PORT}`)
+})
