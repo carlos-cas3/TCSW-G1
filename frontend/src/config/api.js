47901acb1cdebf9963aef92ce8cf7a4b1217f39f
David Cisneros <cisneros02culaca@gmail.com>
2026-07-02 10:46:05 -0500

    Actualiza interfaz de gestión de postventa

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -3,4 +3,5 @@ export const API = {
     VENDORS: import.meta.env.VITE_VENDOR_URL,
     CATALOG: import.meta.env.VITE_CATALOG_URL,
     ORDERS: import.meta.env.VITE_ORDERS_URL,
+    POSTVENTA: import.meta.env.VITE_POSTVENTA_URL,
 };
